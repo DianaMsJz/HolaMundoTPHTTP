be88a9f77a722f33c64b3a94fe7f2d9b6f207e5c
DianaMsJz <dianamsjz@gmail.com>
2021-11-03 11:48:18 -0700

    Hola mundo en HTML usando HTTP

--- hola-mundoTP.js
+++ hola-mundoTP.js
@@ -3,8 +3,8 @@ const hostname = '192.168.56.1';
 const port = 3000;
 const server = http.createServer((req, res)=>{
     res.statusCode=200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hola Mundo\n');
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hola Mundo</h1>');
 });
 
 server.listen(port, hostname, () => {
